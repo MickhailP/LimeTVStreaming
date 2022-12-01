f107fb591e9e6e83a18a91e77a953e0dd33a7409
Mikhail <mich_p@inbox.ru>
2022-12-01 20:51:26 +0300

    Initial Commit

---
+++ LimeTVStreaming/LimeTVStreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimeTVStreamingApp.swift
+//  LimeTVStreaming
+//
+//  Created by Миша Перевозчиков on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LimeTVStreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
